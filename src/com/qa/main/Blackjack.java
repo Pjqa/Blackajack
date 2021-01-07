129ac9925f1a9718e0b986e07aca39f383f3355a
Pjqa <>
2021-01-07 17:45:08 +0000

    added files

---
+++ src/com/qa/main/Blackjack.java
@@ -0,0 +1,22 @@
+package com.qa.main;
+
+public class Blackjack {
+	
+	public static int BlackJack(int card, int card1) {
+	if (card > 21 && card1 > 21) {
+		return 0;
+	} else if (card > 21 || card1 > 21) {
+		if (card > 21) {
+			return card1;
+		}else {
+			return card;
+		}
+	} else {
+		if ((21 - card) < (21 - card1)) {
+			return card;
+		} else {
+			return card1;
+		}
+	}
+}
+}
---
+++ src/com/qa/main/Runner.java
@@ -0,0 +1,13 @@
+package com.qa.main;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		Blackjack bj = new Blackjack();
+		
+		System.out.println( bj.BlackJack(10, 20));
+	}
+	}
+
+
